b1fbfd9a396694cccbbbbb09e7252789618cff8f
Neil Deo <neildeo42@gmail.com>
2025-08-14 19:19:45 +0100

    Chunk implementation + disassembler

--- makefile
+++ makefile
@@ -1,2 +1,14 @@
-clox: main.c common.h
-	gcc -o clox main.c common.h
+clox: main.o chunk.o memory.o debug.o
+	gcc -o clox main.o chunk.o memory.o debug.o
+
+main.o: main.c common.h chunk.h
+	gcc -c main.c
+
+chunk.o: chunk.c chunk.h memory.h
+	gcc -c chunk.c
+
+memory.o: memory.c memory.h
+	gcc -c memory.c
+
+debug.o: debug.c debug.h
+	gcc -c debug.c
---
+++ memory.h
@@ -0,0 +1,25 @@
+#ifndef clox_memory_h
+#define clox_memory_h
+
+#include "common.h"
+
+#define GROW_CAPACITY(capacity) \
+    ((capacity) < 8 ? 8 : (capacity) * 2)
+
+#define GROW_ARRAY(type, pointer, oldCount, newCount)      \
+    (type *)reallocate(pointer, sizeof(type) * (oldCount), \
+                       sizeof(type) * (newCount))
+
+#define FREE_ARRAY(type, pointer, oldCount) \
+    reallocate(pointer, sizeof(type) * (oldCount), 0)
+
+/*
+Reallocates a block of memory starting at the given pointer based on the `oldSize`
+and `newSize` parameters.
+- If newSize == 0, it frees the block
+- Otherwise, it uses realloc to resize the block. If the reallocation fails, the function
+exits the program with exit code 1.
+*/
+void *reallocate(void *pointer, size_t oldSize, size_t newSize);
+
+#endif
