81e5c4b56c073603e3fb087509a927ffbb6b1ad6
Neil Deo <neildeo42@gmail.com>
2025-08-30 09:38:59 +0100

    Implement for statements

--- compiler.c
+++ compiler.c
@@ -88,6 +88,7 @@ static void printStatement();
 static void expressionStatement();
 static void ifStatement();
 static void whileStatement();
+static void forStatement();
 
 // Parser and compiler behaviour
 
@@ -720,6 +721,10 @@ static void statement()
     {
         printStatement();
     }
+    else if (match(TOKEN_FOR))
+    {
+        forStatement();
+    }
     else if (match(TOKEN_IF))
     {
         ifStatement();
@@ -816,6 +821,61 @@ static void whileStatement()
 
     patchJump(exitJump);
     emitByte(OP_POP);
+}
+
+static void forStatement()
+{
+    beginScope();
+    consume(TOKEN_LEFT_PAREN, "Expect '(' after 'for'.");
+
+    if (match(TOKEN_SEMICOLON))
+    {
+        // No initialiser.
+    }
+    else if (match(TOKEN_VAR))
+    {
+        varDeclaration();
+    }
+    else
+    {
+        expressionStatement();
+    }
+
+    int loopStart = currentChunk()->count;
+    int exitJump = -1;
+    if (!match(TOKEN_SEMICOLON))
+    {
+        expression();
+        consume(TOKEN_SEMICOLON, "Expect ';' after loop condition.");
+
+        // Exit loop if condition is false.
+        exitJump = emitJump(OP_JUMP_IF_FALSE);
+        emitByte(OP_POP);
+    }
+
+    if (!match(TOKEN_RIGHT_PAREN))
+    {
+        int bodyJump = emitJump(OP_JUMP);
+        int incrementStart = currentChunk()->count;
+        expression();
+        emitByte(OP_POP);
+        consume(TOKEN_RIGHT_PAREN, "Expect ')' after for clauses.");
+
+        emitLoop(loopStart);
+        loopStart = incrementStart;
+        patchJump(bodyJump);
+    }
+
+    statement();
+    emitLoop(loopStart);
+
+    if (exitJump != -1)
+    {
+        patchJump(exitJump);
+        emitByte(OP_POP); // Remove condition value from stack.
+    }
+
+    endScope();
 }
 
 /*
