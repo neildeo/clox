5ce6dffbf0ddc23943d72fb163a37b23d4c0063d
Neil Deo <neildeo42@gmail.com>
2025-09-18 08:13:39 +0100

    Getters and setters for fields

--- compiler.c
+++ compiler.c
@@ -99,6 +99,7 @@ static void literal(bool canAssign);
 static void and_(bool canAssign);
 static void or_(bool canAssign);
 static void call(bool canAssign);
+static void dot(bool canAssign);
 
 // Statements
 static void statement();
@@ -411,7 +412,7 @@ ParseRule rules[] = {
     [TOKEN_LEFT_BRACE] = {NULL, NULL, PREC_NONE},
     [TOKEN_RIGHT_BRACE] = {NULL, NULL, PREC_NONE},
     [TOKEN_COMMA] = {NULL, NULL, PREC_NONE},
-    [TOKEN_DOT] = {NULL, NULL, PREC_NONE},
+    [TOKEN_DOT] = {NULL, dot, PREC_CALL},
     [TOKEN_MINUS] = {unary, binary, PREC_TERM},
     [TOKEN_PLUS] = {NULL, binary, PREC_TERM},
     [TOKEN_SEMICOLON] = {NULL, NULL, PREC_NONE},
@@ -855,6 +856,23 @@ static void call(bool canAssign)
 {
     uint8_t argCount = argumentList();
     emitBytes(OP_CALL, argCount);
+}
+
+/* Parse a dot expression (get or set) */
+static void dot(bool canAssign)
+{
+    consume(TOKEN_IDENTIFIER, "Expect property name after '.'.");
+    uint8_t name = identifierConstant(&parser.previous);
+
+    if (canAssign && match(TOKEN_EQUAL))
+    {
+        expression();
+        emitBytes(OP_SET_PROPERTY, name);
+    }
+    else
+    {
+        emitBytes(OP_GET_PROPERTY, name);
+    }
 }
 
 // STATEMENTS
