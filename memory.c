1461efda58517015c6a0591950a15a546f938e48
Neil Deo <neildeo42@gmail.com>
2025-09-18 07:55:27 +0100

    Class instances

--- memory.c
+++ memory.c
@@ -66,6 +66,13 @@ static void freeObject(Obj *object)
         FREE(ObjFunction, object);
         break;
     }
+    case OBJ_INSTANCE:
+    {
+        ObjInstance *instance = (ObjInstance *)object;
+        freeTable(&instance->fields);
+        FREE(ObjInstance, object);
+        break;
+    }
     case OBJ_NATIVE:
         FREE(ObjNative, object);
         break;
@@ -181,6 +188,13 @@ static void blackenObject(Obj *object)
         ObjFunction *function = (ObjFunction *)object;
         markObject((Obj *)function->name);
         markArray(&function->chunk.constants);
+        break;
+    }
+    case OBJ_INSTANCE:
+    {
+        ObjInstance *instance = (ObjInstance *)object;
+        markObject((Obj *)instance->klass);
+        markTable(&instance->fields);
         break;
     }
     case OBJ_UPVALUE:
